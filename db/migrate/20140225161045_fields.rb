da53342956bcb6ad97b767178775bbad2317e573
averygoodplanet <averygoodplanet@gmail.com>
2014-02-25 11:34:33 -0600

    added fields for budget, categories

--- db/migrate/20140225161045_fields.rb
+++ db/migrate/20140225161045_fields.rb
@@ -1,4 +1,8 @@
 class Fields < ActiveRecord::Migration
   def change
+    add_money :budgets, :income
+    add_column :budgets, :name, :string
+    add_money :categories, :amount
+    add_column :categories, :name, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140225154738) do
+ActiveRecord::Schema.define(version: 20140225161045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,11 +19,17 @@ ActiveRecord::Schema.define(version: 20140225154738) do
   create_table "budgets", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "income_cents",    default: 0,     null: false
+    t.string   "income_currency", default: "USD", null: false
+    t.string   "name"
   end
 
   create_table "categories", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "amount_cents",    default: 0,     null: false
+    t.string   "amount_currency", default: "USD", null: false
+    t.string   "name"
   end
 
   create_table "users", force: true do |t|
