6a99f029316e83b205963371e931993e0acf39dd
averygoodplanet <averygoodplanet@gmail.com>
2014-02-26 16:45:37 -0600

    non-working user_spec.rb, issue with simulating current_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :budgets, dependent: :destroy
+
+  def create_new_budget(name)
+    # budget = Budget.create(name: name, user: current_user)
+    budget = Budget.create(name: name, user: self.name)
+  end
 end
