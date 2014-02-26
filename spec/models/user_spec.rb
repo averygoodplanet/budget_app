59e7963ea4c54298e3351e3a132e53fe24bf31f6
averygoodplanet <averygoodplanet@gmail.com>
2014-02-26 14:43:28 -0600

    wrote user_spec test for create_new_budget

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#create_new_budget" do
+    before(:each) do
+      @current_user = Fabricate(:user)
+      @current_user.create_new_budget(name: "new budget")
+    end
+
+    it "should create a new budget under current_user with a budget name" do
+      @current_user.budgets.where(name: "new budget").count.should equal(1)
+    end
+
+    it "should create a budget with income of zero" do
+      @current_user.budgets.where(name: "new budget")[0].income should equal(0)
+    end
+
+    it "should create the ten (10) categories under the new budget" do
+      @current_user.budgets.where(name: "new budget")[0].categories.count should equal(10)
+    end
+
+    it "the new budget's categories should have the default 10 category names" do
+      categories = Budget.first.categories
+      category_names = []
+      for i in categories
+        category_names.push(i.name)
+      end
+      category_names.should =~ ["Saving", "Housing", "Utiilities", "Food",
+        "Transportation", "Clothing", "Medical/Health", "Personal", "Recreation", "Debts"]
+    end
+  end
 end
