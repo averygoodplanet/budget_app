63c577ddb1df13f55d8523f4673a7727fa7b7464
averygoodplanet <averygoodplanet@gmail.com>
2014-03-02 20:44:53 -0600

    starting to build html new form

--- spec/controllers/budgets_controller_spec.rb
+++ spec/controllers/budgets_controller_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe BudgetsController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
+  # describe "GET 'index'" do
+  #   it "returns http success" do
+  #     get 'index'
+  #     response.should be_success
+  #   end
   end
 
 end
