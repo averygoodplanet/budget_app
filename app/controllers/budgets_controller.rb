860b5e3b7fd8a5b9be1f4ee82af4eea96c37ff1b
averygoodplanet <averygoodplanet@gmail.com>
2014-03-03 11:02:26 -0600

    un-tested create new budget and redirect to table page

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,5 +1,18 @@
 class BudgetsController < ApplicationController
   def index
     @budgets = current_user.budgets.all
+    #putting "New Budget" form on same page as budget
+    @budget = current_user.budgets.new
+  end
+
+  def create
+    # get params
+    budget_name = params[:budget][:name]
+    #save new budget to database
+    current_user.create_new_budget(budget_name)
+    # redirect to edit page
+
+    #***Test redirect to /budgets
+    redirect_to action: 'index', status: 303
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 BudgetApp::Application.routes.draw do
   get "budgets" => 'budgets#index'
+  post "budgets" => 'budgets#create'
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
