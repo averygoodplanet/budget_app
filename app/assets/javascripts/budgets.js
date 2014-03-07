2d4ad1651cacfb8ba0017ca01cc94b360c67e5f4
averygoodplanet <averygoodplanet@gmail.com>
2014-03-07 17:22:58 -0600

    set autoNumeric lZero to false

--- app/assets/javascripts/budgets.js
+++ app/assets/javascripts/budgets.js
@@ -2,5 +2,5 @@ $(document).ready(function () {
 
   // var test = "hey";
  // debugger;
-  $('input[data-role=money]').autoNumeric('init', {aSign: '$'});
+  $('input[data-role=money]').autoNumeric('init', {aSign: '$', lZero: 'deny'});
 });
