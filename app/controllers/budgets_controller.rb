c3d75b85b97fe49bcbbb51f8205f29527804d2bf
averygoodplanet <averygoodplanet@gmail.com>
2014-03-07 11:05:06 -0600

    saving whole dollars from edit to income

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -32,15 +32,24 @@ class BudgetsController < ApplicationController
     # binding.pry
     # @book = Book.find(params[:id])
     # @book.update_attributes(params[:book])
+    budget_income = params[:budget][:income_cents].tr('$', '').to_f
     @budget = Budget.find(params[:id])
+    # @budget.update_attributes(income_cents: 100)
+    # @budget.update(income_cents: 200)
+    @budget.update(income_cents: budget_income)
+    # @budget.update(name: budget_income)
+    # @budget.update(income_cents: params[:budget][:income_cents])
+    # binding.pry
+    # @budget.income_cents(budget_income)
+    # @budget.save
     # @budget.update_attributes(budget_params)
-    @budget.update(budget_params)
+    # @budget.update(budget_params)
     # binding.pry
     redirect_to budgets_path
   end
 
   private
   def budget_params
-    params.require(:budget).permit(:income_cents, categories_attributes: [:id, :amount_cents])
+    params.require(:budget).permit(:income_cents, categories_attributes: [:name, :id, :amount_cents])
   end
 end
