b679862c892c51069e6c6ebe6872c830f72dd5d9
averygoodplanet <averygoodplanet@gmail.com>
2014-03-08 13:57:14 -0600

    unallocated funds on edit page form load

--- app/assets/javascripts/budgets.js
+++ app/assets/javascripts/budgets.js
@@ -3,4 +3,28 @@ $(document).ready(function () {
   // var test = "hey";
  // debugger;
   $('input[data-role=money]').autoNumeric('init', {aSign: '$', lZero: 'deny'});
+
+  // If the page has the edit-budget form, then once the form is loaded, update
+  // Unallocated Funds.
+  if ($("form.edit_budget").length != 0) {
+    $("form.edit_budget").load(updateUnallocatedFunds());
+  }
+
+  function updateUnallocatedFunds() {
+    var income_string = $('#budget_income_cents').val();
+    var income = parseFloat(income_string.replace('$', ''));
+    var income_cents = parseInt(income * 100);
+
+    var sum_of_spending_cents = 0
+
+    $('[id$=_amount_cents]').each(function() {
+      var category_spending = parseFloat($(this).val().replace('$', ''));
+      var category_spending_cents = parseInt(category_spending * 100);
+      sum_of_spending_cents += category_spending_cents;
+    });
+    var unallocated_cents = income_cents - sum_of_spending_cents;
+    var unallocated_dollars_and_cents = unallocated_cents / 100;
+    $('#unallocatedFunds').val('$'+unallocated_dollars_and_cents);
+  };
+
 });
